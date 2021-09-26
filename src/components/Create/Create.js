4da755483ce71aed7d433a9328c3e8ee365312f3
Lukasz <wyspianski.lukasz@gmail.com>
2021-09-26 12:38:06 +0000

    Add state to Create component, implement submit action (log to console)

--- src/components/Create/Create.js
+++ src/components/Create/Create.js
@@ -1,19 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button, Form } from 'semantic-ui-react'
 
 function Create() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const callMockAPI = ()=>{
+    console.log(firstName + ' ' + lastName);
+  }
   return (
     <div>
       <Form>
         <Form.Field>
           <label>First Name</label>
-          <input placeholder='First Name' />
+          <input onChange={e => setFirstName(e.target.value)} placeholder='First Name' />
         </Form.Field>
         <Form.Field>
           <label>Last Name</label>
-          <input placeholder='Last Name' />
+          <input onChange={e => setLastName(e.target.value)} placeholder='Last Name' />
         </Form.Field>
-        <Button type='submit'>Submit</Button>
+        <Button type='submit' onClick={callMockAPI}>Submit</Button>
       </Form>
     </div>
   )
