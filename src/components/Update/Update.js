3d7b2d56e97d56afc09709a4ec719de8017980aa
Lukasz <wyspianski.lukasz@gmail.com>
2021-09-26 16:39:03 +0000

    Update with HTTP PUT

--- src/components/Update/Update.js
+++ src/components/Update/Update.js
@@ -5,16 +5,18 @@ import axios from 'axios';
 function Update() {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
+  const [id, setId] = useState(null);
 
   function callMockApiWithAxiosPOST() {
 
     const formData = {
       firstName,
-      lastName
+      lastName,
+      id
     };
 
-    const endpointURL = "https://614f39dcb4f6d30017b484f4.mockapi.io/api/v1/people";
-    axios.post(endpointURL, formData)
+    const endpointURL = `https://614f39dcb4f6d30017b484f4.mockapi.io/api/v1/people/${id}`;
+    axios.put(endpointURL, formData)
       .then(response => console.log(response.data))
       .catch(
         (err) => { console.log(err) }
@@ -25,6 +27,7 @@ function Update() {
   useEffect(() => {
     setFirstName(localStorage.getItem('firstName'));
     setLastName(localStorage.getItem('lastName'));
+    setId(localStorage.getItem('id'));
   }, [])
 
   return (
