b81860e4d8b97ba65aa56312403f68da01618923
Lukasz <wyspianski.lukasz@gmail.com>
2021-09-26 12:01:02 +0000

    Add semantic ui form to Create component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
+import Create from './components/Create/Create';
 
 function App() {
   return (
     <div className="App">
       <h3>React CRUD</h3>
-  
+      <Create />
     </div>
   );
 }
 
 export default App;
+
--- src/components/Create/Create.js
+++ src/components/Create/Create.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import './Create.css';
+import React from 'react'
+import { Button, Form } from 'semantic-ui-react'
 
-const Create = () => (
-  <div className="Create">
-    Create Component
-  </div>
-);
+function Create() {
+  return (
+    <div>
+      <Form>
+        <Form.Field>
+          <label>First Name</label>
+          <input placeholder='First Name' />
+        </Form.Field>
+        <Form.Field>
+          <label>Last Name</label>
+          <input placeholder='Last Name' />
+        </Form.Field>
+        <Button type='submit'>Submit</Button>
+      </Form>
+    </div>
+  )
+}
 
-export default Create;
+export default Create
